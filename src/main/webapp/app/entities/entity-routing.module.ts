533598127d0b68b2bc0c1b6507e894dd9c2e0b71
Herve Launay <herve.launay@sii.fr>
2021-04-22 15:46:09 +0200

    Guard angular route

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Authority } from 'app/config/authority.constants';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
       {
         path: 'team',
-        data: { pageTitle: 'Teams' },
+        data: { 
+          pageTitle: 'Teams',
+          authorities: [Authority.ADMIN], 
+        },
         loadChildren: () => import('./team/team.module').then(m => m.TeamModule),
       },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
